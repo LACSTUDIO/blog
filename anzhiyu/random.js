639b1f330d6b9f4e0003f9834d9575f80d0ecdad
LACSTUDIO <lican2935@gmail.com>
2025-02-21 20:59:40 +0800

    Site updated: 2025-02-21 20:59:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/p/Batch/index/","/p/HyperOS_UnlockTool/index/","/p/刷机/adb_fastboot/","/p/ycsj/index/","/p/roottool/index/","/p/刷机/xiaomi_usb/","/p/软件测试/jmeter/","/p/软件测试/loadrunner/"];function toRandomPost(){
+var posts=["/p/HyperOS_UnlockTool/index/","/p/Batch/index/","/p/软件测试/jmeter/","/p/roottool/index/","/p/刷机/adb_fastboot/","/p/软件测试/loadrunner/","/p/ycsj/index/","/p/刷机/xiaomi_usb/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
